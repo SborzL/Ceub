9e51637da1ea7b7f110d2eeb2a315a4151053a5b
Luís Sborz <sborzl555@gmail.com>
2026-03-20 10:43:49 -0300

    Update Carro methods and add new functionality

--- 2º Semestre/Programação Orientada a Objetos/Aulas/Aula_4/Main.java
+++ 2º Semestre/Programação Orientada a Objetos/Aulas/Aula_4/Main.java
@@ -3,7 +3,17 @@ public class Main{
         // Chamada do método construtor
         Carro carro_1 = new Carro("Toyota", "Supra", 1994);
         carro_1.acelerar();
-        System.out.println(carro_1.mostrarMarca());
+        carro_1.acelerar(200.20f);
+        System.out.println(carro_1.getMarca());
+        System.out.println(carro_1.getModelo());
+        carro_1.setAno(2018);
+        carro_1.setAno(2077);
+        System.out.println(carro_1.getAno());
+        carro_1.setMarca("Lamborghini");
+        carro_1.setMarca(null);
+        carro_1.setMarca("");
+        System.out.println(carro_1.getMarca());
+        carro_1.setModelo("Gallardo");
         carro_1.mostrarInfo();
     }
 }
