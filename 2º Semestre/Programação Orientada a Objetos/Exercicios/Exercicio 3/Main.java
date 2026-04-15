6ba8051e076462e6020ffb0e25d92407356b59d4
SborzL <sborzl555@gmail.com>
2026-04-15 01:04:14 -0300

    UPDATE Portifilo DI

--- 2º Semestre/Programação Orientada a Objetos/Exercicios/Exercicio 3/Main.java
+++ 2º Semestre/Programação Orientada a Objetos/Exercicios/Exercicio 3/Main.java
@@ -1,21 +1,38 @@
 // Exercício 3:Introdução à POO
 // Gerenciador de Produto (Estoque Simples)
-// Objetivo: Praticar a criação de uma classe, definição de atributos privados, uso de métodos construtores, implementação de métodos de acesso (Getters/Setters) e criação de métodos de comportamento que validam o estado do objeto (Encapsulamento).
+// Objetivo: Praticar a criação de uma classe, definição de atributos privados, uso de métodos construtores,
+// implementação de métodos de acesso (Getters/Setters) e criação de métodos de comportamento que validam o estado do objeto (Encapsulamento).
 // Enunciado:
 // Crie uma classe chamada Produto que representará um item de uma loja.
 // Esta classe deve possuir os seguintes atributos privados: nome (String), preco (double) e quantidadeEmEstoque (int).
 // Crie um método construtor que receba o nome e o preco do produto. A quantidadeEmEstoque deve sempre iniciar em 0.
-// Crie os métodos Getters e Setters para nome e preco. Crie apenas o Getter para quantidadeEmEstoque (o estoque não deve ser alterado diretamente via Setter).
+// Crie os métodos Getters e Setters para nome e preco.
+// Crie apenas o Getter para quantidadeEmEstoque (o estoque não deve ser alterado diretamente via Setter).
 // Crie dois métodos de comportamento:
 // adicionarEstoque(int quantidade): Soma a quantidade informada ao estoque atual (apenas se a quantidade for maior que zero).
-// vender(int quantidade): Subtrai a quantidade informada do estoque. O método deve verificar se a quantidade solicitada é válida e se há estoque suficiente. Caso não haja, exiba a mensagem: "Estoque insuficiente para a venda."
+// vender(int quantidade): Subtrai a quantidade informada do estoque.
+// O método deve verificar se a quantidade solicitada é válida e se há estoque suficiente.
+// Caso não haja, exiba a mensagem: "Estoque insuficiente para a venda."
 // exibirResumo(): Imprime no console o nome do produto, seu preço e a quantidade atual em estoque.
-// Crie uma classe principal (Main) para testar sua classe Produto. Instancie um produto, adicione estoque, tente vender uma quantidade válida, tente vender uma quantidade maior que o estoque e exiba o resumo ao final.
+// Crie uma classe principal (Main) para testar sua classe Produto.
+// Instancie um produto, adicione estoque, tente vender uma quantidade válida,
+// tente vender uma quantidade maior que o estoque e exiba o resumo ao final.
 // Teste as possibilidades positivas, negativas e casos extremos.
 
 public class Main {
     public static void main(String[] args){
         
-
+        Produto produto_1 = new Produto ("Blush", 64.99);
+        produto_1.exibirResumo();
+        produto_1.adicionarEstoque(10);
+        produto_1.exibirResumo();
+        produto_1.vender(-12);
+        produto_1.vender(5);
+        produto_1.exibirResumo();
+        produto_1.valorEstoque();
+        produto_1.vender(60);
+        produto_1.adicionarEstoque(-5);
+        produto_1.adicionarEstoque(590987);
+        produto_1.exibirResumo();
     }
 }
--- 2º Semestre/Programação Orientada a Objetos/Exercicios/Exercicio 3/Produto.java
+++ 2º Semestre/Programação Orientada a Objetos/Exercicios/Exercicio 3/Produto.java
@@ -1,18 +1,60 @@
 public class Produto {
     private String nome;
     private double preco;
-    private int quantidadeEmEstoque = 0;
+    private int quantidadeEmEstoque;
 
     public Produto(String nome, double preco){
         this.nome = nome;
         this.preco = preco;
+        this.quantidadeEmEstoque = 0;
     }
     
+    //Método de Comportamento
+    public void exibirResumo(){
+        System.out.println("Produto: "+nome);
+        System.out.println("Preço: R$ "+preco);
+        System.out.println(getQuantidadeEmEstoque());   
+    }
+    
+    public void valorEstoque(){
+        double valorTotal = preco * quantidadeEmEstoque;
+        System.out.println("Valor total em estoque: R$ " + valorTotal);
+    }
+a
+
     //Getter
-    public void mostrarQuantidadeEmEstoque(){
-        System.out.println("Quantidade em Estoque: "+quantidadeEmEstoque);
+    public String getQuantidadeEmEstoque(){
+        return "Quantidade em estoque: " +quantidadeEmEstoque;
     }
 
-    //Setter
+    public String getNome() {
+        return "Nome: " +nome;
+    }
 
+    public String getPreco(){
+        return "Preço: R$ " +preco;
+    }
+
+
+    //Setter
+    public void adicionarEstoque(int quantidade){
+        if (quantidade > 0){
+            quantidadeEmEstoque += quantidade;
+            System.out.println("Estoque atualizado com sucesso!");
+            }
+        else 
+            System.out.println("Quantidade inválida, digite um valor maior que zero.");            
+        }
+
+    public void vender(int quantidade){
+        if (quantidade > 0 && quantidade <= quantidadeEmEstoque){
+            quantidadeEmEstoque -= quantidade;
+            System.out.println("Venda realizada com sucesso!");
+            }
+        else if (quantidade > quantidadeEmEstoque)
+            System.out.println("Estoque insuficiente para a venda.");
+        else
+            System.out.println("Quantidade inválida, digite um valor maior que zero.");
+    }
 }
+
