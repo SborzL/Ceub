ccd74713e4f0535bcaf49afdf30a731dcfe0b04c
Luís Sborz <sborzl555@gmail.com>
2026-04-17 10:47:33 -0300

    UPDATE Main.java Aula_6

--- 2º Semestre/Programação Orientada a Objetos/Aulas/Aula_6/app/Main.java
+++ 2º Semestre/Programação Orientada a Objetos/Aulas/Aula_6/app/Main.java
@@ -1,10 +1,33 @@
 package app;
 
-import veiculos.Veiculo;
+import veiculos.aereo.JatoGuerra;
+// import veiculos.Veiculo;
+// import veiculos.terrestre.Terrestre;
+import veiculos.terrestre.Carro;
 
 public class Main{
     public static void main(String[] args){
-    // Veiculo veiculo_1 = new Veiculo();
     // Não é permitido instanciar objeto da classe veiculo
+    // Veiculo veiculo_1 = new Veiculo();
+    //Terrestre terrestre_1 = new Terrestre("Tanque");
+
+    Carro carro_1 = new Carro("Opala", 4, "Gasolina");
+
+    carro_1.acelerar(100);
+    carro_1.mover();
+    carro_1.desacelerar(20);
+    carro_1.mover();
+    carro_1.mostrarInfo();
+
+    Carro carro_2 = new Carro("BYD Dolphin", 4, "Elétrico");
+    carro_2.mostrarInfo();
+
+    JatoGuerra jato_1 = new JatoGuerra("a", 6);
+    jato_1.ganharAltitude(1000);
+    jato_1.acelerar(920);
+    jato_1.mostrarInfo();
+    jato_1.atirarMissil();
+    jato_1.perderAltitude(120);
+    jato_1.mostrarInfo();
     }
 }
